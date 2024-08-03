d612c63b0428707ad2894e377c6ab13dac48a955
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-08-03 17:16:30 -0400

    removing boilerplate - added a new component - about to tailwind as well

---
+++ LikeJira-frontend/src/components/Board.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Board() {
+    return (
+        <React.Fragment >
+            <h1>This is the main board</h1>
+        </React.Fragment>
+    );
+}
+export default Board;
