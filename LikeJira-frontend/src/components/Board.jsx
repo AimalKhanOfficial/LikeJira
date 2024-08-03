eecb563c95de772c26767fd632580e3fe7b64ceb
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-08-03 17:55:23 -0400

    adding swimlanes - basic styling

--- LikeJira-frontend/src/components/Board.jsx
+++ LikeJira-frontend/src/components/Board.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Swimlane from "./Row";
 
 function Board() {
     return (
-        <React.Fragment >
-            <p>Board</p>
-        </React.Fragment>
+        <div className="flex justify-center">
+            <Swimlane title={'To Do'} tickets={[{}]}/>
+            <Swimlane title={'In Progress'} tickets={[{}, {}, {}, {}]} />
+            <Swimlane title={'Ready for Validation'} tickets={[{}]} />
+            <Swimlane title={'Validation'} tickets={[{}]} />
+            <Swimlane title={'Sign off'} tickets={[{}, {}]} />
+            <Swimlane title={'Done/Rejected'} tickets={[{}, {}, {}]} />
+        </div>
     );
 }
 export default Board;
---
+++ LikeJira-frontend/src/components/Row.jsx
@@ -0,0 +1,9 @@
+export default function Swimlane(props) {
+    return (
+        <div className="flex p-[10px]">
+            <div className="h-[800px] w-[250px] max-w-[300px] bg-gray-300 p-[8px]">
+                <p className="opacity-[60%] text-gray-60 uppercase text-[12px]">{props.title} {props.tickets.length}</p>
+            </div>
+        </div>
+    );
+}
